353f8b445ebad00e7721fb923a8d86f3ff28f1aa
ESC <youhavethewrong@gmail.com>
2018-12-02 12:26:20 -0600

    Sloppy solution for part two of day one.

--- src/advent_of_code_2018/day_one.clj
+++ src/advent_of_code_2018/day_one.clj
@@ -3,3 +3,17 @@
 (defn tweak-frequency
   [tweaks]
   (reduce + 0 tweaks))
+
+;; this is bad
+(defn tweak-frequency-detecting-cycles
+  [tweaks]
+  (let [seen (atom #{})
+        done (atom false)]
+    (reduce (fn [acc t]
+              (let [result (+ acc t)]
+                (swap! seen conj acc)
+                (when (contains? @seen result)
+                  (reset! done true))
+                result))
+            0
+            (take-while (fn [_] (not @done)) tweaks))))
