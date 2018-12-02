9b2deb664f0e2fdfa3baa4f77fdebb9d6e11e1ca
ESC <youhavethewrong@gmail.com>
2018-12-02 15:25:04 -0600

    Update comments with what needs improvement

--- src/advent_of_code_2018/day_one.clj
+++ src/advent_of_code_2018/day_one.clj
@@ -1,10 +1,11 @@
 (ns advent-of-code-2018.day-one)
 
+;; problem one
 (defn tweak-frequency
   [tweaks]
   (reduce + 0 tweaks))
 
-;; this is bad
+;; problem two - this could be alot more elegant
 (defn tweak-frequency-detecting-cycles
   [tweaks]
   (let [seen (atom #{})
