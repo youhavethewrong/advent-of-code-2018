2bbd4ea7edfc03db45cdaf66b215b8018f5a8bce
ESC <youhavethewrong@gmail.com>
2018-12-02 21:35:39 -0600

    More code suggestions with kibit!

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cljfmt "0.6.2"]
-            [lein-bikeshed "0.5.1"]]
+            [lein-bikeshed "0.5.1"]
+            [lein-kibit "0.1.6"]]
   :main ^:skip-aot advent-of-code-2018.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
